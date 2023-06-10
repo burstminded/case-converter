3e951b517ecbbe000fb1058814ed516e8ce5d793
burstminded <52548677+crispycrunch22@users.noreply.github.com>
2023-06-10 15:55:22 +0300

    Made request logic in service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConverterService } from './converter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  
+  constructor(public convertService: ConverterService) {}
+
+  ngOnInit(): void {
+    this.convertService.getCurrencies().subscribe()
+  }
+  
+  
   title = 'case-converter';
 }
--- src/app/converter.service.ts
+++ src/app/converter.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, delay, tap } from 'rxjs';
+
+
+export interface currencyReceiveObj{
+  cc: string,
+  rate: string
+
+}
+export interface currencyObj {
+  [key: string]: number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConverterService {
 
-  constructor() { }
+  
+
+  currenciesAvailable: currencyObj[] = [];
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getCurrencies() {
+    return this.http.get<currencyReceiveObj[]>('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+    .pipe(
+      delay(3000),
+      tap(currencies => {
+        for(let i = 0; i < currencies.length; i++) {
+          let currency: currencyReceiveObj = currencies[i];
+          if(currency.cc == 'USD' || currency.cc == 'EUR') {
+            this.currenciesAvailable.push({[currency.cc]:+currency.rate})
+          }
+        }
+        this.currenciesAvailable.push({'UAH':1});
+      })
+    )
+  }
+  
+  
 }
